0babb3756b515b6650ec32f94ec38ee3c2df136f
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-11-15 10:41:10 -0300

    Optimize imports

--- src/api/tasks/dependencies.py
+++ src/api/tasks/dependencies.py
@@ -1,6 +1,5 @@
 from src.domain.tasks.service.todo_tasks_service import TodoTasksService
 from src.infra.tasks.adapter.in_memory_todo_tasks_repository import InMemoryTodoTasksRepository
-from src.infra.user.repository.in_memory_user_repository import InMemoryUserRepository
 
 
 def get_todo_tasks_service() -> TodoTasksService:
