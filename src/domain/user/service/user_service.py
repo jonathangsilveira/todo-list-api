069043d2d0d3a1c4316e649bfbaf33745e84ad38
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-11-29 18:03:52 -0300

    refactor: return user when add or update it

--- src/domain/user/service/user_service.py
+++ src/domain/user/service/user_service.py
@@ -1,8 +1,10 @@
+import logging
 from typing import Optional
 
-from src.domain.user.model.new_user import NewUser
-from src.domain.user.model.user import User
+from src.domain.user.model.user_models import User, UserCreation, UserUpdate
 from src.domain.user.repository.user_repository import UserRepository
+
+logger = logging.getLogger(__name__)
 
 
 class UserService:
@@ -10,8 +12,11 @@ class UserService:
     def __init__(self, user_repository: UserRepository) -> None:
         self._user_repository = user_repository
 
-    async def new_user(self, new_user: NewUser):
-        await self._user_repository.insert_user(new_user)
+    async def new_user(self, new_user: UserCreation) -> User:
+        return await self._user_repository.insert_user(new_user)
+
+    async def update_user(self, user_update: UserUpdate) -> User:
+        return await self._user_repository.set_user(user_update)
 
     async def get_user_by_email(self, email: str) -> Optional[User]:
         return await self._user_repository.get_user_by_email_or_none(email)
--- src/infra/user/repository/in_memory_user_repository.py
+++ src/infra/user/repository/in_memory_user_repository.py
@@ -1,25 +1,40 @@
 import uuid
+from datetime import datetime, timezone
 from typing import Optional
 
-from src.domain.user.model.new_user import NewUser
-from src.domain.user.model.user import User
+from src.domain.user.exception.user_not_found import UserNotFoundException
+from src.domain.user.model.user_models import User, UserCreation, UserUpdate
 from src.domain.user.repository.user_repository import UserRepository
 
 
 class InMemoryUserRepository(UserRepository):
     _users: dict[str, User] = {}
 
-    async def insert_user(self, user: NewUser) -> None:
+    async def insert_user(self, user_creation: UserCreation) -> User:
         user_id = self._generate_uuid()
-        self._users[user.email] = User(
-            uuid=user_id,
-            full_name=user.full_name,
-            email=user.email,
-            password=user.password
+        user = User(
+            id=user_id,
+            full_name=user_creation.full_name,
+            email=user_creation.email,
+            password=user_creation.password,
+            created_at=datetime.now(tz=timezone.utc),
+            is_active=user_creation.is_active
         )
+        self._users[user_creation.email] = user
+        return user
+
+    async def set_user(self, user_update: UserUpdate) -> User:
+        user = self._users.get(user_update.email)
+        if not user:
+            raise UserNotFoundException()
+        user.full_name = user_update.full_name
+        user.password = user_update.password
+        user.is_active = user_update.is_active
+        user.last_update_at = datetime.now(tz=timezone.utc)
+        return user
 
     async def get_user_by_email_or_none(self, email: str) -> Optional[User]:
-        return self._users[email]
+        return self._users.get(email)
 
     async def delete_all_users(self) -> None:
         self._users.clear()
