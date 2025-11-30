7f57d178b77475ce24a196c8ffd29d8adbc07210
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-11-30 11:20:57 -0300

    refactor: add audit fields to user entity

--- src/infra/database/sqlite/model/sqlite_models.py
+++ src/infra/database/sqlite/model/sqlite_models.py
@@ -14,14 +14,20 @@ class UserEntity(BaseEntity):
     full_name = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False)
     password = Column(String(100), nullable=False)
+    created_at = Column(DateTime, unique=False, nullable=False)
+    last_update_at = Column(DateTime, unique=False, nullable=True)
     active = Column(Boolean, nullable=False, default=True)
 
-    def __init__(self, id: str, full_name: str, email: str, password: str, active: bool):
+    def __init__(self, id: str, full_name: str, email: str, password: str, created_at: datetime.datetime,
+                 last_update_at: Optional[datetime.datetime] = None, active: bool = True):
         self.id = id
         self.full_name = full_name
         self.email = email
         self.password = password
+        self.created_at = created_at
+        self.last_update_at = last_update_at
         self.active = active
+
 
 class TodoTaskEntity(BaseEntity):
     __tablename__ = "todo_tasks"
