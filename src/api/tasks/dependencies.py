9b64fff9d1215a7d415fa1fe901c38274050e5db
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-11-30 11:24:15 -0300

    refactor: move repository implementation to adapter repository package

--- src/api/tasks/dependencies.py
+++ src/api/tasks/dependencies.py
@@ -1,5 +1,5 @@
 from src.domain.tasks.service.todo_tasks_service import TodoTasksService
-from src.infra.tasks.adapter.in_memory_todo_tasks_repository import InMemoryTodoTasksRepository
+from src.infra.tasks.adapter.repository.in_memory_todo_tasks_repository import InMemoryTodoTasksRepository
 
 
 def get_todo_tasks_service() -> TodoTasksService:
