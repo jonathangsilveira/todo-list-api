b51b75713d10a50b80aa1f7e42a5dd259db94a0b
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-12-06 18:25:19 -0300

    Revert "refactor: add extra properties to jwt payload model"
    This reverts commit 2b58d87a60ccad37e948b38b3df374653d7d1cb3.

--- src/domain/token/model/token.py
+++ src/domain/token/model/token.py
@@ -1,11 +1,7 @@
-import uuid
 from dataclasses import dataclass
-from datetime import datetime, timezone
 
 
-@dataclass(eq=True, frozen=True)
-class JwtPayload:
-    sub: str
-    exp: datetime
-    iat: datetime = datetime.now(tz=timezone.utc)
-    jti: str = str(uuid.uuid4())
+@dataclass
+class Token:
+    value: str
+    type: str = "Bearer"
