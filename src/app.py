2bce8e93f9bb49a582b93fa62e3a97592ae5fa3e
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-12-08 15:40:15 -0300

    refactor: implement async context manager to manage connections and clients

--- src/app.py
+++ src/app.py
@@ -1,14 +1,25 @@
+from contextlib import asynccontextmanager
+
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from src.api.health import health
 from src.api.auth.v1 import auth
 from src.api.tasks.v1 import tasks
+from src.infra.database.redis.client import create_redis_client
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    load_dotenv()
+    redis_client = await create_redis_client()
+    app.state.redis_client = redis_client
+    yield
+    await redis_client.aclose()
 
 
 def create_fastapi_app() -> FastAPI:
-    load_dotenv()
-    api_app = FastAPI()
+    api_app = FastAPI(lifespan=lifespan)
     api_app.include_router(health.router)
     api_app.include_router(auth.router)
     api_app.include_router(tasks.router)
