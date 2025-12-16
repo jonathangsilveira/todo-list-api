34fdcb1ddf50d0f378c8e8a8c33de18ce8be306d
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-12-16 17:05:59 -0300

    refactor: handle exceptions using core exceptions

--- src/infra/user/adapter/repository/local_users_repository.py
+++ src/infra/user/adapter/repository/local_users_repository.py
@@ -1,10 +1,12 @@
 import traceback
 import uuid
 from datetime import datetime, timezone
+from sqlite3 import IntegrityError
 from typing import Optional
 
 from sqlalchemy.future import select
 
+from src.core.exception.exceptions import InternalErrorException, NotFoundException, ConflictException
 from src.domain.user.exception.user_not_found import UserNotFoundException
 from src.domain.user.model.user_models import UserCreation, User, UserUpdate
 from src.domain.user.repository.user_repository import UserRepository
@@ -25,10 +27,10 @@ class LocalUsersRepository(UserRepository):
             async for session in self._async_session_factory.generate_session():
                 results = await session.execute(statement)
                 user_entity = results.scalars().first()
-                return EntityUserMapper.from_entity(user_entity) if user_entity else None
-        except Exception:
-            details = traceback.format_exc()
-            raise DatabaseReadException(details=details)
+
+            return EntityUserMapper.from_entity(user_entity) if user_entity else None
+        except Exception as exc:
+            raise InternalErrorException(message="Error fetching user by e-mail") from exc
 
     async def insert_user(self, user_creation: UserCreation) -> User:
         try:
@@ -36,48 +38,44 @@ class LocalUsersRepository(UserRepository):
             user_entity = EntityUserMapper.new_user(generated_id, user_creation)
 
             async for session in self._async_session_factory.generate_session():
-                session.add(user_entity)
+                try:
+                    session.add(user_entity)
+                except IntegrityError as exc:
+                    raise ConflictException(message=f"User with e-mail {user_creation.email} already exists!") from exc
 
             return EntityUserMapper.from_entity(user_entity)
-        except Exception:
-            details = traceback.format_exc()
-            raise DatabaseWriteException(details=details)
+        except ConflictException:
+            raise
+        except Exception as exc:
+            raise InternalErrorException(message=f"Error adding user {user_creation.full_name}") from exc
 
     async def set_user(self, user_update: UserUpdate) -> User:
-        async for session in self._async_session_factory.generate_session():
-            try:
-                user_entity: Optional[type[UserEntity]] = await session.get(UserEntity, user_update.id)
-            except Exception:
-                details = traceback.format_exc()
-                raise DatabaseReadException(details=details)
+        try:
+            async for session in self._async_session_factory.generate_session():
+                user_entity = await session.get(UserEntity, user_update.id)
 
-            if not user_entity:
-                raise UserNotFoundException()
+                if not user_entity:
+                    raise NotFoundException(message=f"User ID {user_update.id} not found!")
 
-            try:
                 user_entity.full_name = user_update.full_name
                 user_entity.password = user_update.password
                 user_entity.active = user_update.is_active
                 user_entity.last_update_at = datetime.now(tz=timezone.utc)
-            except Exception:
-                details = traceback.format_exc()
-                raise DatabaseWriteException(details=details)
 
             return EntityUserMapper.from_entity(user_entity)
+        except NotFoundException:
+            raise
+        except Exception as exc:
+            raise InternalErrorException(message=f"Error updating user {user_update.full_name}") from exc
 
     async def delete_all_users(self) -> None:
-        async for session in self._async_session_factory.generate_session():
-            try:
+        try:
+            async for session in self._async_session_factory.generate_session():
                 statement = select(UserEntity)
                 results = await session.execute(statement)
                 all_users = results.scalars().all()
-            except Exception:
-                details = traceback.format_exc()
-                raise DatabaseReadException(details=details)
 
-            try:
                 for user in all_users:
                     await session.delete(user)
-            except Exception:
-                details = traceback.format_exc()
-                raise DatabaseWriteException(details=details)
+        except Exception as exc:
+            raise InternalErrorException(message=f"Error removing all users") from exc
