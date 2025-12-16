2884b5bc202f40ff5001b44fa668b21872684dce
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-12-16 17:07:43 -0300

    refactor: handle exceptions using core exceptions

--- src/infra/tasks/adapter/repository/local_todo_tasks_repository.py
+++ src/infra/tasks/adapter/repository/local_todo_tasks_repository.py
@@ -1,15 +1,14 @@
-import traceback
 from datetime import datetime, timezone
 from typing import Optional
 
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.future import select
 
-from src.domain.tasks.exception.todo_task_not_found import TodoTaskNotFoundException
+from src.core.exception.exceptions import NotFoundException, InternalErrorException
 from src.domain.tasks.model.new_todo_task import NewTodoTask
 from src.domain.tasks.model.todo_task import TodoTask
 from src.domain.tasks.model.todo_task_status import TodoTaskStatus
 from src.domain.tasks.repository.todo_tasks_repository import TodoTasksRepository
-from src.infra.database.exception.exceptions import DatabaseReadException, DatabaseWriteException
 from src.infra.database.sqlite.model.sqlite_models import TodoTaskEntity
 from src.infra.database.sqlite.session.async_session_factory import AsyncSessionFactory
 from src.infra.tasks.mapper.entity_todo_task_mapper import EntityTodoTaskMapper
@@ -17,8 +16,8 @@ from src.infra.tasks.mapper.entity_todo_task_mapper import EntityTodoTaskMapper
 
 class LocalTodoTasksRepository(TodoTasksRepository):
 
-    def __init__(self, _async_session_generator: AsyncSessionFactory):
-        self._async_session_generator = _async_session_generator
+    def __init__(self, async_session_generator: AsyncSessionFactory):
+        self._async_session_generator = async_session_generator
 
     async def get_todo_tasks_by_user(self, user_id: str) -> list[TodoTask]:
         try:
@@ -27,10 +26,9 @@ class LocalTodoTasksRepository(TodoTasksRepository):
                 results = await session.execute(statement)
                 todo_task_entity = results.scalars().all()
                 return [EntityTodoTaskMapper.from_entity(entity) for entity in todo_task_entity]
-        except Exception:
-            details = traceback.format_exc()
-            raise DatabaseReadException(details=details)
-        return []
+            return []
+        except Exception as exc:
+            raise InternalErrorException(message=f"Error fetching TODO tasks by user id {user_id}") from exc
 
     async def get_todo_task_by_uuid(self, uuid: str) -> Optional[TodoTask]:
         try:
@@ -40,21 +38,23 @@ class LocalTodoTasksRepository(TodoTasksRepository):
                 return EntityTodoTaskMapper.from_entity(todo_task_entity) if todo_task_entity else None
 
             return None
-        except Exception:
-            details = traceback.format_exc()
-            raise DatabaseReadException(details=details)
+        except Exception as exc:
+            raise InternalErrorException(message=f"Error fetching TODO task by UUID {uuid}") from exc
 
-    async def add_todo_task(self, user_id: str, todo_task: NewTodoTask) -> TodoTask:
+    async def upsert_todo_task(self, user_id: str, todo_task: NewTodoTask) -> TodoTask:
         try:
             todo_task_entity = EntityTodoTaskMapper.new_entity(todo_task, user_id)
 
             async for session in self._async_session_generator.generate_session():
-                session.add(todo_task_entity)
+                try:
+                    session.add(todo_task_entity)
+                except IntegrityError:
+                    todo_task_entity.updated_at = datetime.now(tz=timezone.utc)
+                    todo_task_entity.last_sync_at = datetime.now(tz=timezone.utc)
 
             return EntityTodoTaskMapper.from_entity(todo_task_entity)
-        except Exception:
-            details = traceback.format_exc()
-            raise DatabaseWriteException(details=details)
+        except Exception as exc:
+            raise InternalErrorException(message=f"Error upserting TODO task {todo_task.uuid}") from exc
 
     async def mark_todo_task_as_done(self, uuid: str) -> TodoTask:
         try:
@@ -62,16 +62,17 @@ class LocalTodoTasksRepository(TodoTasksRepository):
                 todo_task_entity = await session.get(TodoTaskEntity, uuid)
 
                 if not todo_task_entity:
-                    raise TodoTaskNotFoundException()
+                    raise NotFoundException(message=f"TODO task {uuid} not found!")
 
                 todo_task_entity.status = TodoTaskStatus.DONE.value()
                 todo_task_entity.updated_at = datetime.now(tz=timezone.utc)
                 todo_task_entity.last_sync_at = datetime.now(tz=timezone.utc)
 
             return EntityTodoTaskMapper.from_entity(todo_task_entity)
-        except Exception:
-            details = traceback.format_exc()
-            raise DatabaseWriteException(details=details)
+        except NotFoundException:
+            raise
+        except Exception as exc:
+            raise InternalErrorException(message=f"Error updating TODO task by UUID: {uuid}") from exc
 
     async def remove_todo_task_by_uuid(self, uuid: str) -> None:
         try:
@@ -79,9 +80,10 @@ class LocalTodoTasksRepository(TodoTasksRepository):
                 todo_task_entity = await session.get(TodoTaskEntity, uuid)
 
                 if not todo_task_entity:
-                    raise TodoTaskNotFoundException()
+                    raise NotFoundException(message=f"TODO task {uuid} not found!")
 
                 await session.delete(todo_task_entity)
-        except Exception:
-            details = traceback.format_exc()
-            raise DatabaseWriteException(details=details)
+        except NotFoundException:
+            raise
+        except Exception as exc:
+            raise InternalErrorException(message=f"Error removing TODO task by UUID: {uuid}") from exc
