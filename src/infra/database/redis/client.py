604f2504eff42032d2e016febd3055fe6c209a0b
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-12-24 15:57:05 -0300

    refactor: create redis client from url

--- src/infra/database/redis/client.py
+++ src/infra/database/redis/client.py
@@ -4,6 +4,10 @@ from redis.asyncio import Redis
 
 
 def create_redis_client() -> Redis:
+    redis_url = os.getenv("REDIS_URL")
+    if redis_url:
+        return Redis.from_url(url=redis_url, decode_responses=True)
+
     host = os.getenv("REDIS_HOST", "localhost")
     port = int(os.getenv("REDIS_PORT", "6379"))
     db = int(os.getenv("REDIS_DB", "0"))
